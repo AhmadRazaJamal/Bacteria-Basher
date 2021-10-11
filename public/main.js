2d96ca0815bf10041a2104628fbf51c6f44a1325
AhmadRazaJamal <ahmad.raza@me.com>
2021-10-11 18:44:02 -0500

    added a circle to the middle of the canvas

--- public/main.js
+++ public/main.js
@@ -47,93 +47,76 @@ function bacteriaBasher() {
   // Clear the color buffer with specified clear color
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-	gl.viewport(0,0,canvas.width,canvas.height);
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  gl.viewport(canvas.width/4, canvas.height/4, canvas.width/2, canvas.height/2);
 
   /*  
   Create, Compile and link Shaders 
   */
 
   var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-	var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+  var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 
-  gl.shaderSource(vertexShader,vertexShaderText);
-	gl.shaderSource(fragmentShader,fragmentShaderText);
+  gl.shaderSource(vertexShader, vertexShaderText);
+  gl.shaderSource(fragmentShader, fragmentShaderText);
 
   gl.compileShader(vertexShader);
-	if(!gl.getShaderParameter(vertexShader,gl.COMPILE_STATUS)){
-		console.error('Error compiling vertex shader!', gl.getShaderInfoLog(vertexShader))
-		return;
-	}
-	gl.compileShader(fragmentShader);
-		if(!gl.getShaderParameter(fragmentShader,gl.COMPILE_STATUS)){
-		console.error('Error compiling vertex shader!', gl.getShaderInfoLog(fragmentShader))
-		return;
-	}
+  if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+    console.error('Error compiling vertex shader!', gl.getShaderInfoLog(vertexShader))
+    return;
+  }
+  gl.compileShader(fragmentShader);
+  if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+    console.error('Error compiling vertex shader!', gl.getShaderInfoLog(fragmentShader))
+    return;
+  }
 
   var program = gl.createProgram();
-	gl.attachShader(program,vertexShader);
-	gl.attachShader(program,fragmentShader);
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
 
-	gl.linkProgram(program);
-	if(!gl.getProgramParameter(program,gl.LINK_STATUS)){
-		console.error('Error linking program!', gl.getProgramInfo(program));
-		return;
+  gl.linkProgram(program);
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    console.error('Error linking program!', gl.getProgramInfo(program));
+    return;
   }
 
   /*  
   Create a circle buffer
   */
 
-	//all arrays in JS is Float64 by default
-	var triangleVertices = [
-		//X,   Y,     R, G, B
-		Math.cos(0.0),  Math.sin(0.5),    1, 0, 0,
-		Math.cos(-0.5),  Math.sin(-0.5), ,    0, 1, 0,
-		Math.cos(0.5),  Math.sin(-0.5),     0, 0, 1
-	];
+  function radian(degree) {
+    var rad = degree * (Math.PI / 180);
+    return rad;
+  }
 
-	var triangleVertexBufferObject = gl.createBuffer();
-	//set the active buffer to the triangle buffer
-	gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
-	//gl expecting Float32 Array not Float64
-	//gl.STATIC_DRAW means we send the data only once (the triangle vertex position
-	//will not change over time)
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices),gl.STATIC_DRAW);
+  function attributeSetFloats(gl, prog, attr_name, rsize, arr) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(arr),
+      gl.STATIC_DRAW);
+    var attr = gl.getAttribLocation(prog, attr_name);
+    gl.enableVertexAttribArray(attr);
+    gl.vertexAttribPointer(attr, rsize, gl.FLOAT, false, 0, 0);
+  }
 
-	var positionAttribLocation = gl.getAttribLocation(program,'vertPosition');
-	var colorAttribLocation = gl.getAttribLocation(program,'vertColor');
-	gl.vertexAttribPointer(
-		positionAttribLocation, //attribute location
-		2, //number of elements per attribute
-		gl.FLOAT, 
-		gl.FALSE,
-		5*Float32Array.BYTES_PER_ELEMENT,//size of an individual vertex
-		0*Float32Array.BYTES_PER_ELEMENT//offset from the beginning of a single vertex to this attribute
-		);
-	gl.vertexAttribPointer(
-		colorAttribLocation, //attribute location
-		3, //number of elements per attribute
-		gl.FLOAT, 
-		gl.FALSE,
-		5*Float32Array.BYTES_PER_ELEMENT,//size of an individual vertex
-		2*Float32Array.BYTES_PER_ELEMENT//offset from the beginning of a single vertex to this attribute
-		);
-	gl.enableVertexAttribArray(positionAttribLocation);
-	gl.enableVertexAttribArray(colorAttribLocation);
+  function drawCircle() {
+    gl.useProgram(program);
 
-	gl.useProgram(program);
-	
-	 /*  
-    Drawing the circle
-  */
-		
-	gl.clearColor(0.5,0.8,0.8,1.0);
-	gl.clear(gl.COLOR_BUFFER_BIT);	
-	gl.drawArrays(gl.TRIANGLES,0,3);
-	
+    var arrayradian = [];
+    var rotationradian = [];
+ 
+    for (var i = 0; i <= 360; i += 1) {
+      arrayradian.push(radian(i));
+      rotationradian.push(Math.cos(radian(i)), Math.sin(radian(i)), 0);
+    }
 
+    attributeSetFloats(gl, program, "vertPosition", 3, rotationradian);
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, rotationradian.length / 3);
+    //10 the THIRD parameter number of POINT/TWO POINT = VECTOR
+  }
+
+  drawCircle();
 }
 
 window.onload = bacteriaBasher;
